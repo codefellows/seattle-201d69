ddcab842941e33b921e391a9ce2f12b58cb8b7d9
Cristian Robles <formymoment12@gmail.com>
2020-10-30 18:45:39 -0500

    finish question 3

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -9,13 +9,13 @@ Test this function by hand in  the console to get it working, and when you think
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  var add = a + b;
-  var string = 'The sum of ' + a + ' and ' + b + ' is ' + add + '.';
-  return [add, string];
+  var addsum = a + b;
+  var string = 'The sum of ' + a + ' and ' + b + ' is ' + addsum + '.';
+  return [addsum, string];
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+testSum(4, 7,0);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var add = sum(a,sum(b,c)[0])[0];
+  var multi = multiply(a,multiply(b,c)[0])[0];
+  var stringone = a + ' and ' + b + ' and ' + c + ' sum to ' + add + '.';
+  var stringtwo = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multi+'.';
+  return[add,multi,stringone,stringtwo];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
