e6d217dfdfa1d8dbc773e2b311c9ef0538831e81
Cristian Robles <formymoment12@gmail.com>
2020-10-30 19:39:42 -0500

    Finished question five and coded to be pass

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -101,11 +101,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var proOne = multiply(multArr[0],multiply(multArr[1],multArr[2])[0])[0];
+  var proString = 'The numbers '+multArr[0]+','+multArr[1]+','+multArr[2]+' have a product of '+proOne+'.';
+  return [proOne,proString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
