213d0782df81e265317b3a53fc421675cb141583
Sang Lee <sanglee76@yahoo.com>
2020-10-30 20:58:48 -0700

    updated with problemThree completed

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -33,7 +33,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   var product = a * b;
   var productString = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
-  return[product, productString];
+  return [product, productString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -54,11 +54,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var myList = [a, b, c];
+  var sumCollector = sum(myList[0], myList[1]);
+  var sumOutput = sum(sumCollector[0],myList[2]);
+  var productCollector = multiply(myList[0], myList[1]);
+  var productOutput = multiply(productCollector[0], myList[2]);
+  var sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOutput[0] + '.';
+  var productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOutput[0] + '.';
+  return [sumOutput[0], productOutput[0], sumString, productString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
