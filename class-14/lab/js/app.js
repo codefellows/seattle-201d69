75a08806b17ec250654daf03e9df5a31409d8ac5
Sang Lee <sanglee76@yahoo.com>
2020-11-12 14:57:01 -0800

    updated progress on app.js and catalog.js

--- class-14/lab/js/app.js
+++ class-14/lab/js/app.js
@@ -8,15 +8,19 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  new CartItem(product, quantity); // * added
+  this.items.push(CartItem); // * added
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.cart = JSON.stringify(this.items); // * added
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+
 };
 
 var CartItem = function(product, quantity) {
--- class-14/lab/js/catalog.js
+++ class-14/lab/js/catalog.js
@@ -10,9 +10,11 @@ var cart = new Cart([]);
 function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
-  var selectElement = document.getElementById('items');
-  for (var i in Product.allProducts) {
-
+  var selectElement = document.getElementById('items');           // * added
+  for (var i=0; i < Product.allProducts.length; i++) {            // * added
+    var productOption = document.createElement('option', value=Product.allProducts[i].name);         // * added
+    productOption.textContent = Product.allProducts[i].name;      // * added
+    selectElement.appendChild(productOption);                    // * added
   }
 
 }
@@ -23,7 +25,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+  event.preventDefault(); // * added
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
@@ -37,6 +39,12 @@ function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  var selectElement = document.getElementById('items');           // * added
+  var itemSelected = selectElement.value;
+  var quantityElement = document.getElementById('quantity');
+  var itemAmount = quantityElement.value;
+  new CartItem(itemSelected, itemAmount);
+
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
